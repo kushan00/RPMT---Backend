284b82199ccb0bf3b39ae28e69c6a40485923b4b
NethmiPrabodhika <prabodhikanethmi@gmail.com>
2022-05-27 15:45:55 +0530

    topic updated changes

--- Routes/topicRoutes.js
+++ Routes/topicRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {getTopics , getTopicById ,createTopic} = require("../Controllers/topicController");
+const {getTopics , getTopicById ,createTopic,updateTopic} = require("../Controllers/topicController");
 
 
 router.post("/createTopic",createTopic);
 router.get("/getAllTopics",getTopics);
 router.get("/getTopicById/:id",getTopicById);
+router.patch("/updateTopic/:id",updateTopic);
 
 
 
