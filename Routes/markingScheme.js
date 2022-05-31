15a1f183e3b5dde4563484e2aefc13a3154496e5
Diniru Sandipa <it20041120@my.sliit.lk>
2022-05-31 14:29:46 +0530

    diniru controllers added

--- Routes/markingScheme.js
+++ Routes/markingScheme.js
@@ -1,30 +1,12 @@
 const router = require("express").Router();
 const File = require('../Model/markingScheme');
 
+const {newMarkingScheme, allMarkings} = require ("../Controllers/markingSchemeController");
+
 // upload new file
-router.post("/uploadmarking", async (req, res) => {
-    try {
-        const file = await File(req.body).save();
-
-        res.status(201).send({data: file, message: "File Uploaded successfully" ,status:201})
-
-    } catch (error) {
-        res.status(500).send({message: "Internal Server Error"})
-
-    }
-})
+router.post("/uploadmarking", newMarkingScheme)
 
 // Get all files
-router.get("/allmarkings", async (req, res) => {
-    try {
-        const files = await File.find();
-
-        res.status(200).send({data: files})
-
-    } catch (error) {
-        res.status(500).send({message: "Internal Server Error"})
-
-    }
-})
+router.get("/allmarkings", allMarkings)
 
 module.exports = router;
